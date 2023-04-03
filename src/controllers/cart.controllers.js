4100e8be062ef91b593d1af4259d5652837dc89f
Layenx Laventure <llayenx@gmail.com>
2023-04-03 20:35:11 -0300

    add product to cart

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -1,3 +1,5 @@
+const Cart = require("../models/carts.models")
+const ProductInCart = require("../models/productInCarts.models")
 const cartServices = require("../services/cart.services")
 
 const createCart = async(req, res, next)=>{
@@ -21,9 +23,25 @@ const getAllCarts= async(req, res, next)=>{
         
     }
 }
+// uso este controlador sin servicio importando los modelos necesarios
+const addProductToCart = async(req, res, next)=>{
+    try {
+        const {cartId, productId, quantity, price}= req.body
+        await ProductInCart.create({cartId, productId, quantity, price})
+        const totalPrice = price * quantity
+        await Cart.increment({totalPrice}, {where :{id: cartId}})
+        res.json({
+            message: "product added"
+        })
+    } catch (error) {
+        next(error)
+        
+    }
+}
 
 
 module.exports = {
     createCart,
     getAllCarts,
+    addProductToCart
 }
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,8 +1,9 @@
 const {Router}=  require("express")
-const{createCart, getAllCarts} = require("../controllers/cart.controllers")
+const{createCart, getAllCarts, addProductToCart} = require("../controllers/cart.controllers")
 const router = Router ()
 
 router.post("/api/v1/carts", createCart)
 router.get("/api/v1/carts", getAllCarts)
+router.post("/api/v1/carts/add-product", addProductToCart)
 
 module.exports = router
